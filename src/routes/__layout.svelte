4d5f32d14a43e0db4f6da448d56441988baf1327
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-02-08 16:11:16 +0900

    chore: implement tailwind css

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import '$lib/styles/tailwind.css';
+</script>
+
+<slot />
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./src/app.html', './src/**/*.{svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+};
