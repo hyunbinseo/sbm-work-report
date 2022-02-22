4131169150815f9a13ce1e19ecbd2d40cc7d7b5c
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-02-22 17:53:06 +0900

    feat: mock report preview table

--- src/lib/components/Report.svelte
+++ src/lib/components/Report.svelte
@@ -1,3 +1,38 @@
 <div class="overflow-x-auto">
-	<!-- ToDo: Create table -->
+	<table class="min-w-full divide-y divide-gray-200 whitespace-nowrap">
+		<!-- Reference https://www.w3.org/WAI/tutorials/tables/irregular/ -->
+		<col />
+		<col />
+		<col />
+		<colgroup span="3" />
+		<colgroup span="3" />
+		<tr>
+			<th scope="col" rowspan="2">번호</th>
+			<th scope="col" rowspan="2">요원</th>
+			<th scope="col" rowspan="2">출장<br />횟수</th>
+			<th scope="colgroup" colspan="3">보수 미공제</th>
+			<th scope="colgroup" colspan="3">보수 공제</th>
+		</tr>
+		<tr>
+			<th scope="col">연가</th>
+			<th scope="col">교육</th>
+			<th scope="col">병가</th>
+			<th scope="col">병가</th>
+			<th scope="col">이탈</th>
+			<th scope="col">결근</th>
+		</tr>
+		<tr>
+			<th scope="row">1번</th>
+			<th scope="row">
+				<a href="#1">홍길동</a>
+			</th>
+			<td>1회</td>
+			<td>2일</td>
+			<td>-</td>
+			<td>-</td>
+			<td>3일</td>
+			<td>-</td>
+			<td>-</td>
+		</tr>
+	</table>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import Select from '$lib/components/elements/Select.svelte';
+
 	import Agent from '$lib/components/Agent.svelte';
 	import Divider from '$lib/components/Divider.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import Report from '$lib/components/Report.svelte';
-	import Select from '$lib/components/elements/Select.svelte';
 
 	import { gangnam } from '$lib/data/organizations';
 
@@ -53,10 +54,10 @@
 				<div class="mt-10 lg:mt-0">
 					<h2 class="text-lg font-medium text-gray-900">보고서</h2>
 					<div
-						class="mt-4 overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm sm:sticky sm:top-6 lg:top-8"
+						class="mt-4 space-y-6 overflow-hidden rounded-lg border border-gray-200 bg-white py-6 px-4 shadow-sm sm:sticky sm:top-6 sm:px-6 lg:top-8"
 					>
 						<Report />
-						<div class="border-t border-gray-200 py-6 px-4 sm:px-6">
+						<div class="border-t border-gray-200">
 							<button
 								type="submit"
 								class="w-full rounded-md border border-transparent bg-indigo-600 py-3 px-4 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
