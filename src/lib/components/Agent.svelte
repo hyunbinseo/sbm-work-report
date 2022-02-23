88d588838cd38fff6484d7ad1a244c9675623a6d
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-02-23 18:15:16 +0900

    feat: update agent form input attributes

--- src/lib/components/Agent.svelte
+++ src/lib/components/Agent.svelte
@@ -6,7 +6,9 @@
 	on:submit|preventDefault={() => {}}
 	class="mt-6 overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm"
 >
-	<div class="grid grid-cols-2 gap-y-6 gap-x-4 p-4 sm:grid-cols-3 sm:px-6 sm:pt-5 sm:pb-7">
+	<div
+		class="grid grid-cols-2 gap-y-6 gap-x-4 p-4 sm:grid-cols-3 sm:px-6 sm:pt-5 sm:pb-7 xl:grid-cols-4"
+	>
 		<Input label="이름" name="name" placeholder="홍길동" type="text" />
 		<Input label="출장 횟수" name="business-trip" type="number" />
 		<Input label="연가 일수" name="annual-leave" type="number" />
@@ -18,8 +20,8 @@
 			type="number"
 			placeholder="30일 초과"
 		/>
-		<Input label="이탈 일수 (공제)" name="deduct-leave" type="number" />
-		<Input label="결근 일수 (공제)" name="deduct-absence" type="number" />
+		<Input label="이탈 일수 (공제)" name="deduct-leave" type="number" value={0} />
+		<Input label="결근 일수 (공제)" name="deduct-absence" type="number" value={0} />
 	</div>
 	<nav
 		class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6"
@@ -30,7 +32,7 @@
 		</div>
 		<div class="flex flex-1 justify-between sm:justify-end">
 			<button
-				type="button"
+				type="reset"
 				class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
 			>
 				초기화
--- src/lib/components/elements/Input.svelte
+++ src/lib/components/elements/Input.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
+	import { keepInputValue } from '$lib/utilities/form';
+
 	export let label: string;
 	export let name: string;
-	export let placeholder: string = '';
 	export let type: 'text' | 'number';
+
+	export let required = true;
+	export let placeholder: string = '';
+	export let value: string | number = null;
+
+	// Currently used to input number of days only
+	const attributes = type === 'number' ? { min: 0 } : {};
 </script>
 
 <div>
-	<label for="cvc" class="block font-medium text-gray-700">
+	<label class="block font-medium text-gray-700">
 		{label}
 		<input
-			{type}
 			{name}
+			{type}
+			{required}
 			{placeholder}
+			{...attributes}
+			use:keepInputValue={value === null ? null : value.toString()}
 			class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
 		/>
 	</label>
