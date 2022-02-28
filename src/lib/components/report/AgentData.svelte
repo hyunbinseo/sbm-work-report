bd18432f6d485717d4fcf2c1cf5370ae66a44ed9
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-02-28 16:54:43 +0900

    feat: show detailed agent leave data on print

--- src/lib/components/report/AgentData.svelte
+++ src/lib/components/report/AgentData.svelte
@@ -19,14 +19,22 @@
 			<th scope="col" rowspan="2">번호</th>
 			<th scope="col" rowspan="2">요원</th>
 			<th scope="col" rowspan="2">출장<br />횟수</th>
-			<th scope="colgroup" colspan="2">보수 지급</th>
+			<th scope="colgroup" colspan={!print ? 2 : 6}>보수 지급</th>
 			<th scope="colgroup" colspan="3">보수 미지급</th>
 			{#if !print}
 				<th scope="col" rowspan="2">비고</th>
 			{/if}
 		</tr>
 		<tr class="bg-gray-100">
-			<th scope="col">휴가</th>
+			{#if !print}
+				<th scope="col">휴가</th>
+			{:else}
+				<th scope="col">연가</th>
+				<th scope="col">병가</th>
+				<th scope="col">공가</th>
+				<th scope="col">청원<br />휴가</th>
+				<th scope="col">특별<br />휴가</th>
+			{/if}
 			<th scope="col">교육</th>
 			<th scope="col">결근</th>
 			<th scope="col">병가</th>
@@ -34,7 +42,7 @@
 		</tr>
 		{#if !$agentStore.length}
 			<tr>
-				<td colspan="9" class="text-center">입력된 보수 정보가 없습니다.</td>
+				<td colspan={!print ? 9 : 12} class="text-center">입력된 보수 정보가 없습니다.</td>
 			</tr>
 		{:else}
 			{#each $agentStore as agent, index (agent.id)}
@@ -42,7 +50,15 @@
 					<th scope="row">{index + 1}</th>
 					<th scope="row">{agent.name}</th>
 					<td>{agent.businessTrip}회</td>
-					<td>{Object.values(agent.paid).reduce((a, b) => a + b)}일 </td>
+					{#if !print}
+						<td>{Object.values(agent.paid).reduce((a, b) => a + b)}일 </td>
+					{:else}
+						<td>{agent.paid.annualLeave}일</td>
+						<td>{agent.paid.sickLeave}일</td>
+						<td>{agent.paid.officialLeave}일</td>
+						<td>{agent.paid.emergencyLeave}일</td>
+						<td>{agent.paid.specialLeave}일</td>
+					{/if}
 					<td>{agent.education}일</td>
 					<td>{agent.unpaid.exceedLeave}일</td>
 					<td>{agent.unpaid.sickLeave}일</td>
