f055767aedacc318bbb68f4b08b4626606aa176a
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-03-01 22:53:35 +0900

    feat: inform macOS Safari users about print

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	const { VITE_TITLE } = import.meta.env;
 
+	import { onMount } from 'svelte';
 	import { v4 as uuidv4 } from 'uuid';
 
 	import AgentData from '$lib/components/report/AgentData.svelte';
@@ -15,6 +16,16 @@
 	const setDocumentId = () => (documentId = uuidv4());
 
 	setDocumentId();
+
+	let isSafariOnMac = false;
+
+	onMount(() => {
+		if (
+			/(Macintosh).*?(Safari).*?/.test(window.navigator.userAgent) &&
+			window.navigator.vendor === 'Apple Computer, Inc.'
+		)
+			isSafariOnMac = true;
+	});
 </script>
 
 <svelte:window
@@ -69,6 +80,13 @@
 						>
 							인쇄하기
 						</button>
+						{#if isSafariOnMac}
+							<p class="text-xs text-gray-500">
+								macOS Safari에서는 위 버튼이 <a
+									href="https://github.com/crabbly/Print.js/issues/528">1회만 작동</a
+								>합니다. 이후에는 메뉴 막대 > 파일 > 프린트 메뉴를 이용합니다.
+							</p>
+						{/if}
 					</div>
 				</div>
 			</div>
