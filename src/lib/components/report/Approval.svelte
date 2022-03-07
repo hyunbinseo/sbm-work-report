9bd6b80b393a000480aab16618f0f4e19d051a0d
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-03-07 18:05:47 +0900

    feat: add team leader in approval table

--- src/lib/components/report/Approval.svelte
+++ src/lib/components/report/Approval.svelte
@@ -2,14 +2,14 @@
 	<tr class="bg-gray-100">
 		<th scope="col" rowspan="2">결재</th>
 		<th>담당</th>
+		<th>팀장</th>
 		<th>과장</th>
 	</tr>
 	<tr>
-		<th class="p-0">
-			<div class="h-[2cm] w-[2cm]" />
-		</th>
-		<th class="p-0">
-			<div class="h-[2cm] w-[2cm]" />
-		</th>
+		{#each new Array(3) as _}
+			<th class="p-0">
+				<div class="h-[2cm] w-[2cm]" />
+			</th>
+		{/each}
 	</tr>
 </table>
