a946a2e88df37328897f11e8444ff2ded1bbbbd3
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-03-15 19:03:06 +0900

    chore: update footer styling and message

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,14 +1,19 @@
 <footer aria-labelledby="꼬리말" class="border-t border-gray-200 bg-white">
 	<div class="mx-auto max-w-screen-2xl px-4 sm:px-6 lg:px-12 xl:px-16">
-		<div class="py-10 text-center lg:flex lg:items-center lg:justify-between">
-			<p class="mb-3 text-gray-700 lg:mb-0">
-				본 웹페이지의 저작권자는 <a on:click|stopPropagation href="https://github.com/hyunbinseo"
-					>서현빈</a
-				>입니다.
+		<div
+			class="py-10 text-center text-sm text-gray-600 lg:flex lg:items-center lg:justify-between lg:text-base"
+		>
+			<p class="mb-3 lg:mb-0">
+				사회복무요원 관리 개선을 위해 <a
+					target="_blank"
+					rel="noopener"
+					href="https://github.com/hyunbinseo/">서현빈</a
+				>이 제작했습니다.
 			</p>
-			<p class="text-gray-700">
+			<p>
 				오류 및 개선 사항을 <a
-					on:click|stopPropagation
+					target="_blank"
+					rel="noopener"
 					href="https://github.com/hyunbinseo/sbm-work-report/discussions/1">알려주세요.</a
 				>
 			</p>
